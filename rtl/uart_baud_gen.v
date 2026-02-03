182930f7a091d3c806ccd40351050de8f1ee6a14
nguyen-24805 <nguyenpktdbha@gmail.com>
2026-02-03 17:13:20 +0700

    Update code

--- rtl/uart_baud_gen.v
+++ rtl/uart_baud_gen.v
@@ -20,7 +20,50 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module uart_baud_gen(
+module uart_baud_gen (
+    input  wire        clk,
+    input  wire        rst,
+    input  wire [31:0] baud_division,
+    input  wire        en,
+    output reg         baud_tick
+);
 
-    );
+    reg [31:0] baud_count;
+    reg        en_d;
+
+    // edge detect
+    always @(posedge clk) begin
+        if (rst)
+            en_d <= 0;
+        else
+            en_d <= en;
+    end
+
+    wire en_q = en & ~en_d;
+
+    // baud counter
+    always @(posedge clk) begin
+        if (rst) begin
+            baud_count <= 0;
+            baud_tick  <= 0;
+        end
+        else begin
+            baud_tick <= 0;   // default 
+
+            if (!en || baud_division == 0) begin
+                baud_count <= 0;
+            end
+            else if (en_q) begin
+                baud_count <= 0;
+            end
+            else if (baud_count == baud_division - 1) begin
+                baud_count <= 0;
+                baud_tick  <= 1;
+            end
+            else begin
+                baud_count <= baud_count + 1;
+            end
+        end
+    end
+
 endmodule
